33d4b60b737a0d2e6f176fbba8cee743e6ff15a3
YH <yinghuihu@gmail.com>
2013-04-13 20:39:49 -0400

    check in with log4php

--- PHPHelloworld/test.php
+++ PHPHelloworld/test.php
@@ -1,4 +1,21 @@
 <?php
-echo "test";
-echo "this is a test";
+include('log4php/Logger.php');
+ 
+ error_reporting(E_ALL);
+ 
+// Tell log4php to use our configuration file.
+Logger::configure('config.xml');
+ 
+// Fetch a logger, it will inherit settings from the root logger
+$log = Logger::getLogger('myLogger');
+ 
+// Start logging
+$log->trace("My first message.");   // Not logged because TRACE < WARN
+$log->debug("My second message.");  // Not logged because DEBUG < WARN
+$log->info("My third message.");    // Not logged because INFO < WARN
+$log->warn("My fourth message.");   // Logged because WARN >= WARN
+$log->error("My fifth message.");   // Logged because ERROR >= WARN
+$log->fatal("My sixth message.");   // Logged because FATAL >= WARN
+
+echo "hello";
 ?>
