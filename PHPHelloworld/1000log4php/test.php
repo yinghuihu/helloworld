d06a8bb318f1d433b085376812299de8a9713a1e
YH <yinghuihu@gmail.com>
2013-04-13 20:47:26 -0400

    add log4php test sample

--- PHPHelloworld/1000log4php/test.php
+++ PHPHelloworld/1000log4php/test.php
@@ -1,10 +1,10 @@
 <?php
-include('log4php/Logger.php');
+include('../log4php/Logger.php');
  
  error_reporting(E_ALL);
  
 // Tell log4php to use our configuration file.
-Logger::configure('config.xml');
+Logger::configure('../config.xml');
  
 // Fetch a logger, it will inherit settings from the root logger
 $log = Logger::getLogger('myLogger');
@@ -13,7 +13,7 @@ $log = Logger::getLogger('myLogger');
 $log->trace("My first message.");   // Not logged because TRACE < WARN
 $log->debug("My second message.");  // Not logged because DEBUG < WARN
 $log->info("My third message.");    // Not logged because INFO < WARN
-$log->warn("My fourth message.");   // Logged because WARN >= WARN
+$log->warn("My 4th message.");   // Logged because WARN >= WARN
 $log->error("My fifth message.");   // Logged because ERROR >= WARN
 $log->fatal("My sixth message.");   // Logged because FATAL >= WARN
 
